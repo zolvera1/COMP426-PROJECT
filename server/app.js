ca34f4ac96c72a423dbf2c3f6eecd77a6bdb22a2
Sam Gomez Olvera <samyolvera1@gmail.com>
2019-10-30 15:02:57 -0400

    refractored server file and added html files

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
